4abd01daeead6047459a292eb1a7d0ca96ec7b5a
Rbtistab <russbatisbarrrco@gmail.com>
2024-08-31 19:06:13 -0600

    CDK initialization

---
+++ test/dopplekopf-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DopplekopfCdk from '../lib/dopplekopf-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dopplekopf-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DopplekopfCdk.DopplekopfCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
